d7e664c9674d7a1f747d45504d00a5064b256d3f
AshwinH <ashwinh@cybage.com>
2015-09-17 14:21:10 +0530

    Fixes comment typo

--- routes/v1/jshint.api.js
+++ routes/v1/jshint.api.js
@@ -6,8 +6,7 @@ var router = express.Router();
 var jshint = require('../../config/npm.config').jshint;
 
 /**
- * Access URL:
- * localhost:9000/api/v1/js/errors
+ * Access URL: localhost:9000/api/v1/lint/js/errors
  */
 router.get('/js/errors', function(req, res, next) {
 	res.json({
