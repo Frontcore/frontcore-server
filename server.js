3b1eb9fe4ae046e1c63aa5c4b9119334a368e71a
hegdeashwin <ashwin.hegde3@gmail.com>
2015-09-17 18:27:10 +0530

    Adds a self calling function

--- server.js
+++ server.js
@@ -1,50 +1,65 @@
-'use strict';
+(function() {
+	'use strict';
 
-var http = require('http'),
-	app = require('./app'),
-	PRODUCT = require('./package.json');
+	/**
+	 * Requires a in-built utility functions;
+	 */
+	var http = require('http');
 
-/**
- * Create HTTP server.
- */
-var server = http.createServer(app);
+	/**
+	 * Requires a constants utility functions;
+	 */
+	var PRODUCT = require('./package.json');
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(app.get('port'));
+	/**
+	 * Requires a frontcore utility functions;
+	 * @requires app:./app.js
+	 */
+	var app = require('./app');
 
-server.on('error', onError);
-server.on('listening', onListening);
+	/**
+	 * Create HTTP server.
+	 */
+	var server = http.createServer(app);
 
-/**
- * Event listener for HTTP server 'error' event.
- */
-function onError(error) {
-	if (error.syscall !== 'listen') {
-		throw error;
+	/**
+	 * Listen on provided port, on all network interfaces.
+	 */
+	server.listen(app.get('port'));
+
+	server.on('error', onError);
+	server.on('listening', onListening);
+
+	/**
+	 * Event listener for HTTP server 'error' event.
+	 */
+	function onError(error) {
+		if (error.syscall !== 'listen') {
+			throw error;
+		}
+
+		/**
+		 * Handle specific listen errors with friendly message.
+		 */
+		switch (error.code) {
+			case 'EACCES':
+				console.error('Requires elevated privileges');
+				process.exit(1);
+				break;
+			case 'EADDRINUSE':
+				console.error('Port is already in use');
+				process.exit(1);
+				break;
+			default:
+				throw error;
+		}
 	}
 
 	/**
-	 * Handle specific listen errors with friendly message.
+	 * Event listener for HTTP server 'listening' event.
 	 */
-	switch (error.code) {
-		case 'EACCES':
-			console.error('Requires elevated privileges');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error('Port is already in use');
-			process.exit(1);
-			break;
-		default:
-			throw error;
+	function onListening() {
+		console.log('\n\n\t %s v%s is listening on server %s:%s', PRODUCT.name, PRODUCT.version, app.get('uri'), app.get('port'));
 	}
-}
 
-/**
- * Event listener for HTTP server 'listening' event.
- */
-function onListening() {
-	console.log('\n\n\t %s v%s is listening on server %s:%s', PRODUCT.name, PRODUCT.version, app.get('uri'), app.get('port'));
-}
+})();
