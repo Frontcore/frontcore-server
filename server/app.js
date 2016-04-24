43293cc175b401a00e3ffbac46612e9a5428ae8a
hegdeashwin <ashwin.hegde3@gmail.com>
2016-04-24 13:09:19 +0530

    Re-factors

--- server/app.js
+++ server/app.js
@@ -1,84 +1,81 @@
-(function() {
-    'use strict';
+'use strict';
 
-    /**
-     * Requires a in-built utility functions;
-     */
-    var path = require('path'),
-        fs = require('fs');
+/**
+ * Requires a in-built utility functions;
+ */
+var path = require('path');
+var fs = require('fs');
 
-    /**
-     * Requires a 3rd party utility functions;
-     */
-    var express = require('express'),
-        morgan = require('morgan'),
-        bodyParser = require('body-parser'),
-        compression = require('compression');
+/**
+ * Requires a 3rd party utility functions;
+ */
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var compression = require('compression');
 
-    /**
-     * Requires a constants utility functions;
-     */
-    var STACK_CONFIG = require("../config/stack.conf");
+/**
+ * Requires a constants utility functions;
+ */
+var STACK_CONFIG = require("../config/stack.conf");
 
-    /**
-     * Create an express app;
-     */
-    var app = express();
+/**
+ * Create an express app;
+ */
+var app = express();
 
-    /**
-     * Create a write stream (in append mode)
-     */
-    var accessLogStream = fs.createWriteStream(__dirname + '/' + STACK_CONFIG.logger.dirname + '/' + STACK_CONFIG.logger.filename, {
-        flags: 'a'
-    });
+/**
+ * Create a write stream (in append mode)
+ */
+var accessLogStream = fs.createWriteStream(__dirname + '/' + STACK_CONFIG.logger.dirname + '/' + STACK_CONFIG.logger.filename, {
+    flags: 'a'
+});
 
-    /**
-     * Setup the logger
-     */
-    app.use(morgan('combined', {
-        stream: accessLogStream
-    }));
+/**
+ * Setup the logger
+ */
+app.use(morgan('combined', {
+    stream: accessLogStream
+}));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    app.use(compression());
-    app.set('trust proxy', function(ip) {
-        return (ip === '127.0.0.1') ? true : false;
-    });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(compression());
+app.set('trust proxy', function(ip) {
+    return (ip === '127.0.0.1') ? true : false;
+});
 
-    var NODE_ENV = process.env.NODE_ENV || STACK_CONFIG.server.dev.NODE_ENV;
-    switch (NODE_ENV.toLowerCase()) {
-        case STACK_CONFIG.server.dev.NODE_ENV.toLowerCase():
-            /**
-             * Application configurations for development environment.
-             * NODE_ENV=development node server.js
-             */
-            app.set('port', process.env.PORT || STACK_CONFIG.server.dev.port);
-            app.set('uri', STACK_CONFIG.server.dev.ip);
-            app.use(express.static(path.join(__dirname, STACK_CONFIG.server.dev.codebase)));
-            break;
+var NODE_ENV = process.env.NODE_ENV || STACK_CONFIG.server.dev.NODE_ENV;
+switch (NODE_ENV.toLowerCase()) {
+    case STACK_CONFIG.server.dev.NODE_ENV.toLowerCase():
+        /**
+         * Application configurations for development environment.
+         * NODE_ENV=development node server.js
+         */
+        app.set('port', process.env.PORT || STACK_CONFIG.server.dev.port);
+        app.set('uri', STACK_CONFIG.server.dev.ip);
+        app.use(express.static(path.join(__dirname, STACK_CONFIG.server.dev.codebase)));
+        break;
 
-        case STACK_CONFIG.server.prod.NODE_ENV.toLowerCase():
-            /**
-             * Application configurations for production environment.
-             * NODE_ENV=production node server.js
-             */
-            app.set('port', process.env.PORT || STACK_CONFIG.server.prod.port);
-            app.set('uri', STACK_CONFIG.server.prod.ip);
-            app.use(express.static(path.join(__dirname, STACK_CONFIG.server.prod.codebase)));
-            break;
-    }
+    case STACK_CONFIG.server.prod.NODE_ENV.toLowerCase():
+        /**
+         * Application configurations for production environment.
+         * NODE_ENV=production node server.js
+         */
+        app.set('port', process.env.PORT || STACK_CONFIG.server.prod.port);
+        app.set('uri', STACK_CONFIG.server.prod.ip);
+        app.use(express.static(path.join(__dirname, STACK_CONFIG.server.prod.codebase)));
+        break;
+}
 
-    /**
-     * Syntax:
-     * app.use('/api/<version>/', require('./routes/<version>/<api-file>'));
-     */
-    app.use('/api/' + STACK_CONFIG.api.defaults.version + '/lint', require('./routes/' + STACK_CONFIG.api.defaults.version + '/jshint.api'));
+/**
+ * Syntax:
+ * app.use('/api/<version>/', require('./routes/<version>/<api-file>'));
+ */
+app.use('/api/' + STACK_CONFIG.api.defaults.version + '/lint', require('./routes/' + STACK_CONFIG.api.defaults.version + '/jshint.api'));
 
-    // require('./models/jshint.model');
+// require('./models/jshint.model');
 
-    module.exports = app;
-
-})();
+module.exports = app;
--- server/routes/v1/jshint.api.js
+++ server/routes/v1/jshint.api.js
@@ -1,71 +1,22 @@
-(function() {
-	'use strict';
+'use strict';
 
-	/**
-	 * Requires a 3rd party utility functions;
-	 */
-	var express = require('express'),
-		scanWith = require('jshint'),
-		assign = require('object.assign');
+/**
+ * Requires a 3rd party utility functions;
+ */
+var express = require('express');
+var router = express.Router();
 
-	/**
-	 * Create a new router
-	 */
-	var router = express.Router();
+/**
+ * Require static services data
+ */
+var JSHINT_RULES = require('../../services/jshint.service');
 
-	/**
-	 * Require static services data
-	 */
-	var JSHINT_RULES = require('../../services/jshint.service');
-
-	/**
-	 * Require utilities
-	 */
-	var scan = require('../../utils/fsscan');
-
-	router.post('/js/errors', function(req, res) {
-
-		// var files = scan.directory(payload, {
-		// 	match: /.js$/
-		// });
-
-		// console.log(files);
-
-		// scanWith.JSHINT(files, assign({}, JSHINT_RULES.options.jshint.enforcing, JSHINT_RULES.options.jshint.relaxing), JSHINT_RULES.options.jshint.environments);
-
-		// res.json({
-		// 	"files": files,
-		// 	"length": files.length,
-		// 	"jshint": {
-		// 		"errors": scanWith.JSHINT.data().errors
-		// 	}
-		// });
-
-		// var payload = req.body;
-
-
-		// dir.readFiles(payload.jsdir, {
-		// 	match: /.js$/
-		// }, function(err, content, next) {
-		// 	if (err) {
-		// 		throw err;
-		// 	}
-		// 	next();
-		// }, function(err, files) {
-		// 	if (err) throw err;
-
-		// 	scanWith.JSHINT(files, assign({}, JSHINT_RULES.options.jshint.enforcing, JSHINT_RULES.options.jshint.relaxing), JSHINT_RULES.options.jshint.environments);
-
-		// 	res.json({
-		// 		"files": files,
-		// 		"length": files.length,
-		// 		"jshint": {
-		// 			"errors": scanWith.JSHINT.data().errors
-		// 		}
-		// 	});
-		// });
-
+router.get('/js/rules', function(req, res, next) {
+	console.log('1');
+	res.status(200).json({
+		"status": true,
+		"rules": JSHINT_RULES
 	});
+});
 
-	module.exports = router;
-})();
+module.exports = router;
