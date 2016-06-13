e7e4ea5efa873f023bb7d3cc5f48f35f7a1e9b15
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-14 02:06:49 +0530

    Add CreateProject component

---
+++ src/components/CreateProject/CreateProject.jsx
@@ -0,0 +1,32 @@
+'use strict';
+
+import React from 'react';
+import { Button, Panel, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+class MyProjects extends React.Component {
+  render() {
+    return (
+      <Panel header="Create a new project">
+        <Form>
+          <FormGroup controlId="projectName">
+            <ControlLabel>Specify the project name:</ControlLabel>
+            <FormControl type="text" placeholder="E.g. frontcore"/>
+          </FormGroup>
+          <FormGroup controlId="projectName">
+            <ControlLabel>Describe the project:</ControlLabel>
+            <FormControl type="text" placeholder="E.g. A tool to understand technical debt in practice"/>
+          </FormGroup>
+          <FormGroup controlId="formControlsFile">
+            <ControlLabel>Upload frontcore configuration file:</ControlLabel>
+            <FormControl type="file" />
+          </FormGroup>
+          <Button type="submit" bsStyle="success">
+            Create Project
+          </Button>
+        </Form>
+      </Panel>
+    );
+  }
+};
+
+module.exports = MyProjects;
