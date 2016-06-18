9a1cc50699b57d41ca37b47f3b938e411cb2e35f
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-18 19:17:26 +0530

    Adds viewfile sub-component

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -4,21 +4,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Panel, Form, FormGroup, FormControl } from 'react-bootstrap';
 import BrowseList from './BrowseList.jsx';
+import Viewfile from './Viewfile.jsx';
 
 class Browse extends React.Component {
   render() {
     let header = (
-      <h3>Browse project files</h3>
+      <h3>List of project files</h3>
     );
 
     return (
       <Panel className="browse-component" header={header}>
-        <Form>
-          <FormGroup>
-            <FormControl type="text" placeholder="Filter the file" />
-          </FormGroup>
-        </Form>
-        <BrowseList />
+        <div className="col-md-4 pull-left-padding">
+          <Form>
+            <FormGroup>
+              <FormControl type="text" placeholder="Filter the file" />
+            </FormGroup>
+          </Form>
+          <BrowseList />
+        </div>
+        <div className="col-md-8 pull-right-padding">
+          <Viewfile />
+        </div>
       </Panel>
     );
   }
