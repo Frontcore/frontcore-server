4db9e334c09f800d57a67a31fc807dd9e60a7831
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-19 18:37:35 +0530

    Adds Toolbar component

--- src/components/Browse/Viewfile.jsx
+++ src/components/Browse/Viewfile.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Panel } from 'react-bootstrap';
 import brace from 'brace';
 import AceEditor from 'react-ace';
+import Toolbar from './Toolbar.jsx';
 
 import 'brace/mode/json';
 import 'brace/theme/github';
@@ -19,6 +20,7 @@ class Viewfile extends React.Component {
   render() {
     return (
       <Panel header="\project\src\file.js">
+        <Toolbar />
         <AceEditor mode="json" theme="github" name="paramsEditor" height="350px" fontSize={16} width="100%" value={json} editorProps={{$blockScrolling: true}} />
       </Panel>
     );
