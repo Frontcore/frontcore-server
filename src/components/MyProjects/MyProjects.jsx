a8743b82620c1eed684fc2dc223a5198aac6353a
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-16 23:28:57 +0530

    Adds partial MyProjects component

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,60 +1,66 @@
 'use strict';
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import './MyProjects.less';
 import { Panel, Form, FormGroup, FormControl, Label } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as myProjectActions from '../../actions/myProjectsActions';
 
-class MyProjects extends React.Component {
+class MyProjectsComponent extends React.Component {
+  componentDidMount() {
+    this.props.actions.loadMyProjects();
+
+    this.myProjectRow = this.myProjectRow.bind(this);
+  }
+
+  myProjectRow(myProject, index) {
+    let buildLink = myProject.link + "/dashboard";
+    return (
+      <Link key={index} to={buildLink} className="list-group-item">
+        <h4 className="list-group-item-heading">{myProject.name} <Label>v{myProject.version}</Label></h4>
+        <p className="list-group-item-text">{myProject.describe}</p>
+        <p className="list-group-item-text project-location"><strong>Location: </strong>{myProject.path}</p>
+      </Link>
+    );
+  }
+
   render() {
-    let Projects = [{
-      name: "Elastic-hub",
-      describe: "An elasticsearch plugin",
-      version: "1.3.2",
-      path: 'C:\\Project\\elastic-hub',
-      link: "/elastic-hub"
-    }, {
-      name: "Protocore",
-      describe: "Front-end stack for jQuery projects",
-      version: "1.0.0",
-      path: 'C:\\Project\\Protocore',
-      link: "/protocore"
-    }, {
-      name: "Nebula",
-      describe: "Front-end stack for Backbone.js projects",
-      version: "4.5.3",
-      path: 'C:\\Project\\Nebula',
-      link: "/nebula"
-    }];
-
     let header = (
       <h3>My projects</h3>
     );
 
     return (
-      <Panel  className="my-projects-component" header={header}>
+      <Panel className="my-projects-component" header={header}>
         <Form>
           <FormGroup controlId="filterProject">
             <FormControl type="text" placeholder="Filter your project"/>
           </FormGroup>
         </Form>
         <div className="list-group">
-          {
-            Projects.map((data, i) => {
-              let buildLink = data.link + "/dashboard";
-              return (
-                <Link key={i} to={buildLink} className="list-group-item">
-                  <h4 className="list-group-item-heading">{data.name} <Label>v{data.version}</Label></h4>
-                  <p className="list-group-item-text">{data.describe}</p>
-                  <p className="list-group-item-text project-location"><strong>Location: </strong>{data.path}</p>
-                </Link>
-              );
-            })
-          }
+          {this.props.myProjects.map(this.myProjectRow)}
         </div>
       </Panel>
     );
   }
 };
 
-module.exports = MyProjects;
+MyProjectsComponent.propTypes = {
+  myProjects: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    myProjects: state.myProjects
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(myProjectActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyProjectsComponent);
