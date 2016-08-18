4ea45360797e83096504675708f852c23834a641
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-19 01:07:53 +0530

    Fixes #106

---
+++ src/actions/browseActions.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import * as types from '../constants/actionTypes';
+import fetch from 'isomorphic-fetch';
+
+function browsePostRes(browse, json) {
+  return {
+    type: types.BROWSE_RES_POST,
+    browse,
+    posts: json.project.files.children
+  };
+}
+
+export function browseProjectDir(browse) {
+  return (dispatch) => {
+    fetch('/api/v1/browse/project/files', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify(browse)
+    }).then(res => {
+      res.json().then(function(data) {
+         dispatch(browsePostRes(browse, data));
+      });
+    }).catch(error => {
+      console.error(error);
+    });
+  };
+};
--- src/reducers/browseReducer.js
+++ src/reducers/browseReducer.js
@@ -6,8 +6,8 @@ import * as types from '../constants/actionTypes';
 export default function browseReducer(state = [], action) {
 
   switch (action.type) {
-    case types.BROWSE_SUCCESS:
-      return action.browse;
+    case types.BROWSE_RES_POST:
+      return action.posts;
 
     default:
       return state;
