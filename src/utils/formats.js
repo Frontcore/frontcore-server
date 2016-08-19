24c86f8cb98b569c66850f2a9e4e7a1628058a2a
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-19 23:26:35 +0530

    Adds Format utility class

---
+++ src/utils/formats.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Format {
+
+  size(bytes, decimals = 3) {
+    if(!bytes) {
+      return '0 Byte';
+    }
+
+    let k = 1000; // or 1024 for binary
+    let dm = decimals;
+    let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    let i = Math.floor(Math.log(bytes) / Math.log(k));
+  
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
+}
+
+module.exports = Format;
