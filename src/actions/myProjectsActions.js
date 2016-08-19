41c7da84b783330e029ec586e620802987a52648
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-19 22:18:48 +0530

    Re-factors myProjects components

--- src/actions/myProjectsActions.js
+++ src/actions/myProjectsActions.js
@@ -4,10 +4,17 @@ import * as types from '../constants/actionTypes';
 import fetch from 'isomorphic-fetch';
 
 export function loadMyProjects() {
-  return dispatch => fetch('/api/v1/projects/list')
-    .then(res => res.json())
-    .then(
-      myProjects => dispatch({ type: types.LOAD_MY_PROJECTS_SUCCESS, myProjects }),
-      error => dispatch({ type:types.LOAD_MY_PROJECTS_ERROR, error })
-    );
+  return (dispatch) => {
+    fetch('/api/v1/projects/list')
+      .then(res => {
+        res.json().then(function(myProjects) {
+          dispatch({
+            type: types.LOAD_MY_PROJECTS,
+            myProjects
+          });
+        });
+      }).catch(error => {
+        console.error(error);
+      });
+  };
 };
