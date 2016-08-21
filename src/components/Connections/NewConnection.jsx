c2519495f40492efea02031696418be4ded7ece0
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-21 22:15:15 +0530

    Re-factors Connection component

--- src/components/Connections/NewConnection.jsx
+++ src/components/Connections/NewConnection.jsx
@@ -2,42 +2,43 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Panel, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { Button, Panel, Form, FormGroup, FormControl, ControlLabel, Radio, Checkbox } from 'react-bootstrap';
 
 class NewConnection extends React.Component {
   render() {
     let header = (
-      <h3>Create a new MongoDB connection</h3>
+      <h3>MongoDB connection configuration</h3>
     );
 
     return (
       <Panel className="new-connection-component" header={header}>
-        <Form>
-          <div className="col-md-6">
+        <Form className="row">
+          <div className="col-md-12">
             <FormGroup controlId="formControlsFile">
               <ControlLabel>Connection name:</ControlLabel>
               <FormControl type="text" placeholder="Enter text" />
             </FormGroup>
+          </div>
+          <div className="col-md-4">
+            <FormGroup controlId="formControlsFile">
+              <ControlLabel>Hostname:</ControlLabel>
+              <FormControl type="text" defaultValue="127.0.0.1" placeholder="Enter text" />
+            </FormGroup>
+          </div>
+          <div className="col-md-4">
+            <FormGroup controlId="formControlsFile">
+              <ControlLabel>Database name:</ControlLabel>
+              <FormControl type="text" disabled defaultValue="frontcoredb" placeholder="Enter text" />
+            </FormGroup>
+          </div>
+          <div className="col-md-4">
             <FormGroup controlId="formControlsFile">
               <ControlLabel>Port:</ControlLabel>
               <FormControl type="text" defaultValue="27017" placeholder="Enter text" />
             </FormGroup>
           </div>
-          <div className="col-md-6">
-            <FormGroup controlId="formControlsFile">
-              <ControlLabel>Hostname:</ControlLabel>
-              <FormControl type="text" defaultValue="127.0.0.1" placeholder="Enter text" />
-            </FormGroup>
-            <FormGroup controlId="formControlsFile">
-              <ControlLabel>Database name:</ControlLabel>
-              <FormControl type="text" defaultValue="frontcoredb" placeholder="Enter text" />
-            </FormGroup>
-          </div>
-          <hr/>
-
           <div className="col-md-12">
             <Button type="submit" bsStyle="primary">Connect</Button>
-            <Button type="submit" bsStyle="default">Advanced Settings</Button>
           </div>
         </Form>
       </Panel>
--- src/views/Settings/Connection.jsx
+++ src/views/Settings/Connection.jsx
@@ -3,15 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NewConnection from '../../components/Connections/NewConnection.jsx';
-import ConnectionList from '../../components/Connections/ConnectionList.jsx';
 
 class MongoConf extends React.Component {
   render() {
     return (
-      <div class="row">
-        <NewConnection />
-        <ConnectionList />
-      </div>
+      <NewConnection />
     );
   }
 };
