57cc102b8159f57694e4b4e292dd2dd115143a73
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-31 22:21:04 +0530

    Fixes #146

--- server/utils/mongodb.utils.js
+++ server/utils/mongodb.utils.js
@@ -5,6 +5,24 @@ export class MongoDB {
     this.baseUrl = params.baseUrl;
     this.dbPort = params.dbPort;
     this.dbName = params.dbName;
+
+    /**
+     * In-case MongoDB connection throws an error
+     */
+    mongoose.connection.on('error', (error) => {
+      console.error('\n Mongoose default MongoDB connection error');
+      console.error(' More Info on error: ', error);
+    });
+
+    /**
+     * In-case the Node process ends, close the Mongoose connection
+     */
+    process.on('SIGINT', function() {
+      mongoose.connection.close(function () {
+        console.log('\n Mongoose default MongoDB connection disconnected through app termination');
+        process.exit(0);
+      });
+    });
   }
 
   connect() {
