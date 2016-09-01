0db4c05639ed1eb8bd70f0f5762e5b9a27e22c19
hegdeashwin <ashwin.hegde3@gmail.com>
2016-09-01 22:54:46 +0530

    Adds build-in validation for Mongoose model

--- server/models/project.model.js
+++ server/models/project.model.js
@@ -2,18 +2,40 @@ import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 
 let projectInfoSchema = new Schema({
-  "fieldname": { type: String, default: "config" },
-  "originalname": { type: String, default: "frontcore.json" },
+  "fieldname": {
+    type: String,
+    default: "config",
+    required: true
+  },
+  "originalname": {
+    type: String,
+    default: "frontcore.json",
+    required: true
+  },
   "encoding": String,
-  "mimetype": { type: String, default: "application/json" },
+  "mimetype": {
+    type: String,
+    default: "application/json",
+    required: true
+  },
   "destination": String,
   "filename": String,
   "path": String,
   "size": Number,
-  "name": String,
-  "version": { type: String, default: "0.0.1" },
+  "name": {
+    type: String,
+    required: true
+  },
+  "version": {
+    type: String,
+    default: "0.0.1",
+    required: true
+  },
   "description": String,
-  "location": String,
+  "location": {
+    type: String,
+    required: true
+  },
   "createdOn": { type: Date, default: Date.now }
 });
 
