08cf1ab124ce5f3443029535edb7f0e6206ca0d5
hegdeashwin <ashwin.hegde3@gmail.com>
2016-09-12 00:22:07 +0530

    Adds elasticsearch conf + updates mongo+stack conf

---
+++ config/elasticsearch.conf.js
@@ -0,0 +1,10 @@
+/**
+ * Holds Elasticsearch configuration
+ * @module elasticsearch
+ */
+module.exports = {
+  "connect": {
+    "url": "localhost",
+    "port": 9200
+  }
+};
--- config/stack.conf.js
+++ config/stack.conf.js
@@ -1,32 +1,32 @@
-(function() {
-    'use strict';
-
-    module.exports = {
-        "server": {
-            "dev": {
-                "port": "9000",
-                "ip": "127.0.0.1",
-                "codebase": "../dist",
-                "NODE_ENV": "development"
-            },
-            "prod": {
-                "port": "8000",
-                "ip": "127.0.0.1",
-                "codebase": "../dist",
-                "NODE_ENV": "production"
-            }
-        },
-        "api": {
-            "defaults": {
-                "version": "v1"
-            }
-        },
-        "logger": {
-            "dirname": "logs",
-            "filename": "access.log"
-        },
-        "upload": {
-          "dirname": "upload"
-        }
-    };
-})();
+/**
+ * Holds client/server stack configuration
+ * @module stack
+ */
+module.exports = {
+  "server": {
+    "dev": {
+      "port": "9000",
+      "ip": "127.0.0.1",
+      "codebase": "../dist",
+      "NODE_ENV": "development"
+    },
+    "prod": {
+      "port": "8000",
+      "ip": "127.0.0.1",
+      "codebase": "../dist",
+      "NODE_ENV": "production"
+    }
+  },
+  "api": {
+    "defaults": {
+      "version": "v1"
+    }
+  },
+  "logger": {
+    "dirname": "logs",
+    "filename": "access.log"
+  },
+  "upload": {
+    "dirname": "upload"
+  }
+};
