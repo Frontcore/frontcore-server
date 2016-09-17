6d5c4a8dbfd01c73b1e90ba22daab0c3a7a20a9a
hegdeashwin <ashwin.hegde3@gmail.com>
2016-09-17 20:19:26 +0530

    Adds Mongoose model for project directory

---
+++ server/models/projectdir.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+let Schema = mongoose.Schema;
+
+/**
+ * Extended schema for project sub-directory
+ */
+let childrenSchema = new Schema({
+  "projPath": String
+  "name": String,
+  "isFile": Boolean,
+  "size": Number,
+  "extension": String,
+  "children": Array
+});
+
+/**
+ * Project directory schema
+ */
+let projDirSchema = new Schema({
+  "projPath": String,
+  "name": String,
+  "isFile": Boolean,
+  "children": [ childrenSchema ]
+});
+
+/**
+ * Project directory Mongoose model schema
+ * @module ProjectDir
+ * @param {String} ProjectDir - model name
+ * @param {Object} projDirSchema - schema object
+ * @param {String} projectdir - collection name
+ * @returns {Object} ProjectDir Mongoose model
+ */
+module.exports = mongoose.model('ProjectDir', projDirSchema, 'projectdir');
