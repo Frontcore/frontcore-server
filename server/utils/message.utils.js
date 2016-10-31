e827367c8ebfca8c763f9f0b7d6555cbee5efa97
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-01 02:33:32 +0530

    Adds basic validation + re-factors message system

---
+++ server/utils/message.utils.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "required": {
+    "username": "You must enter a username.",
+    "firstname": "You must enter your first name.",
+    "lastname": "You must enter your last name.",
+    "email": "You must enter an email address.",
+    "password": "You must enter a password."
+  }
+};
