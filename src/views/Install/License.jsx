386ab37c368ef3d2de63589c9b5124a8ed13120a
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-07 21:42:53 +0530

    Integrate AlertBox component with License view

--- src/views/Install/License.jsx
+++ src/views/Install/License.jsx
@@ -1,25 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AlertBox from '../../components/AlertBox/AlertBox.jsx';
 
 export default class License extends React.Component {
   constructor(props) {
     super(props);
 
+    this.alertMsg = null;
+
+    this.state = {
+      shouldAlert: false,
+      alertType: "danger"
+    };
+
     this.validate = this.validate.bind(this);
+    this.resetAlertState = this.resetAlertState.bind(this);
   }
 
   validate() {
     let _hasUserAgreed = ReactDOM.findDOMNode(this).querySelectorAll("input[name=agreement]");
-    return _hasUserAgreed[0].checked;
+
+    if (_hasUserAgreed[0].checked) {
+      return true;
+    }
+
+    this.alertMsg = (
+      <div>
+        <h4><i className="fa fa-lg fa-exclamation-circle" aria-hidden="true"></i> Application Error</h4>
+        <p>You must accept the license terms before you go ahead with user account and database setup.</p>
+      </div>
+    );
+
+    this.setState({
+      shouldAlert: true
+    });
+
+    return false;
+  }
+
+  resetAlertState() {
+    this.setState({
+      shouldAlert: false
+    });
   }
 
   render() {
+    let { shouldAlert } = this.state;
+
     return (
       <div className="wizard-license">
         <div className="container license-component">
+          {shouldAlert &&
+            <AlertBox type={this.state.alertType} showAlert={this.state.shouldAlert}>
+              {this.alertMsg}
+            </AlertBox>
+          }
+
           <div className="row">
             <div className="col-md-12">
-              <h5>Be sure to carefully read and understand all the rights and restrictions described in the license terms. You must accept the license terms before you can install and setup user account and database.</h5>
+              <h5>Be sure to carefully read and understand all the rights and restrictions described in the license terms. You must accept the license terms before you go ahead with user account and database setup.</h5>
             </div>
           </div>
 
@@ -45,13 +84,13 @@ export default class License extends React.Component {
               </pre>
               <div className="radio">
                 <label>
-                  <input type="radio" name="agreement" class="agree" value="true"/>
+                  <input type="radio" name="agreement" onClick={this.resetAlertState} class="agree" value="true"/>
                   I have read and accept the license terms.
                 </label>
               </div>
               <div className="radio">
                 <label>
-                  <input type="radio" name="agreement" class="disagree" value="false"/>
+                  <input type="radio" name="agreement" onClick={this.resetAlertState} class="disagree" value="false"/>
                   I do not accept the license terms.
                 </label>
               </div>
